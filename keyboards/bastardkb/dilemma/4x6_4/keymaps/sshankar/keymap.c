0f2af2b42f8fc2adc97480992d84c105b77c100f
Shiv Shankar Menon <shiv@pobox.com>
2024-02-18 13:53:56 -0500

    switch mute with gui key

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/sshankar/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/sshankar/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                         KC_LALT, KC_BSPC,  KC_SPC,   LOWER,      RAISE,  KC_ENT, KC_DEL,  KC_MUTE
+                         KC_LALT, KC_BSPC,  KC_SPC,   LOWER,      RAISE,  KC_ENT, KC_DEL,  KC_RGUI
   //                    ╰───────────────────────────────────╯ ╰───────────────────────────────────╯
   ),
 
